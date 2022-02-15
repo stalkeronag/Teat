ea4f7fd744fc20a1ecc4069a0505bfc5f8ed31f0
stalkeronag <ovovouovryms@gmail.com>
2022-02-15 22:42:52 +0300

    clone repos

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,7 +10,7 @@ namespace Test
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+          
         }
     }
 }
