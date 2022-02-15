35de80339c3afccf069065cef40ce1b18c303356
stalkeronag <ovovouovryms@gmail.com>
2022-02-15 22:26:36 +0300

    HelloWad

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,6 +10,7 @@ namespace Test
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
